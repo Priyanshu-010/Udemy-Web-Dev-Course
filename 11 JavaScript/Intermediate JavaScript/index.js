982076f150b1d1ab7a068e3971bb8dc816c18549
Priyanshu-010 <priyanshurai2772@gmail.com>
2024-12-28 15:58:26 +0530

    Intermediate Star pattern

--- 11 JavaScript/Intermediate JavaScript/index.js
+++ 11 JavaScript/Intermediate JavaScript/index.js
@@ -1,4 +1,28 @@
-let n = Math.random();
-n = (n*6)+ 1;
-n= Math.floor(n);
-console.log(n);
+//01 Random Function
+
+// let n = Math.random();
+// n = (n*6)+ 1;
+// n= Math.floor(n);
+// console.log(n);
+
+//02 Leap Year
+
+// function isLeap(year){
+//   if (year % 4 === 0 && year % 100 !== 0 || year % 400 === 0){
+//     console.log(`${year} is a leap year`);
+//   } else {  
+//     console.log(`${year} is not a leap year`);
+//   }
+// }
+
+// isLeap(2000);
+
+//03 Star Pattern
+
+// function starPattern(n){
+//   for(let i = 0; i < (n + 1); i++){
+//     console.log("*".repeat(i));
+//   }
+// }
+
+// starPattern(5);
