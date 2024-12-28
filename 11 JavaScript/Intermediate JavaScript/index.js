9fd058ce56c428e981b99f3406ed1b62e2d3ca3a
Priyanshu-010 <priyanshurai2772@gmail.com>
2024-12-28 19:33:12 +0530

    Commented everything

--- 11 JavaScript/Intermediate JavaScript/index.js
+++ 11 JavaScript/Intermediate JavaScript/index.js
@@ -94,16 +94,16 @@
 
 //08 Fibonacci Sequence
 
-function fibonacci(n) {
-  let fib = [0, 1];
-  if (n === 0) {
-    return 0;
-  } else {
-    for (let i = 2; i < n; i++) {
-      fib[i] = fib[i - 1] + fib[i - 2];
-    }
-    return fib;
-  }
-}
+// function fibonacci(n) {
+//   let fib = [0, 1];
+//   if (n === 0) {
+//     return 0;
+//   } else {
+//     for (let i = 2; i < n; i++) {
+//       fib[i] = fib[i - 1] + fib[i - 2];
+//     }
+//     return fib;
+//   }
+// }
 
-console.log(fibonacci(5));
+// console.log(fibonacci(5));
