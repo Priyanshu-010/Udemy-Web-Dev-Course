52d8ec54a9b8ffaca06d06caa67984f89dd55881
Priyanshu-010 <priyanshurai2772@gmail.com>
2024-12-30 22:45:06 +0530

    Drum

---
+++ 02 JavaScript/Advance JavaScript/Drum Kit Starting Files/index.js
@@ -0,0 +1,36 @@
+
+//1st Method ForEach Loop
+
+// const drum = document.querySelectorAll(".drum");
+// drum.forEach((btn) =>{
+//   btn.addEventListener("click", btnClick);
+// })
+
+//2nd Method For Loop
+
+// const drum = document.querySelectorAll(".drum").length;
+// for (let i = 0; i <drum; i++){
+//   document.querySelectorAll(".drum")[i].addEventListener("click", btnClick);
+// }
+
+//3rd Method
+
+// const wbtn = document.querySelector(".w");
+// wbtn.addEventListener("click", btnClick);
+// const abtn = document.querySelector(".a");
+// abtn.addEventListener("click", btnClick);
+// const sbtn = document.querySelector(".s");
+// sbtn.addEventListener("click", btnClick);
+// const dbtn = document.querySelector(".d");
+// dbtn.addEventListener("click", btnClick);
+// const jbtn = document.querySelector(".j");
+// jbtn.addEventListener("click", btnClick);
+// const kbtn = document.querySelector(".k");
+// kbtn.addEventListener("click", btnClick);
+// const lbtn = document.querySelector(".l");
+// lbtn.addEventListener("click", btnClick);
+
+//Function
+// function btnClick(){
+// alert("I got Clicked");
+// }
