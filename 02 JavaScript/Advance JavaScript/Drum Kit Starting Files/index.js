0499293330dfac73b9c465587565b097dd5415e8
Priyanshu-010 <priyanshurai2772@gmail.com>
2024-12-31 16:20:44 +0530

    Drum kit project Completed

--- 02 JavaScript/Advance JavaScript/Drum Kit Starting Files/index.js
+++ 02 JavaScript/Advance JavaScript/Drum Kit Starting Files/index.js
@@ -108,7 +108,7 @@
 // }
 
 
-// Main project
+//         ************ Main Project **************** 
 
 const drum = document.querySelectorAll(".drum").length;
 for (let i = 0; i <drum; i++){
@@ -161,5 +161,51 @@ for (let i = 0; i <drum; i++){
         break;
     }
   });
-}
 
+
+  }
+
+
+document.addEventListener("keydown", function(event){
+  // console.log(event)
+  switch (event.key) {
+    case "w":
+        let w = new Audio("./sounds/tom-1.mp3");
+        w.play();
+      break;
+     
+    case "a":
+        let a = new Audio("./sounds/tom-2.mp3");
+        a.play();
+      break;
+
+    case "s":
+        let s = new Audio("./sounds/tom-3.mp3");
+        s.play();  
+      break;
+
+    case "d": 
+        let d = new Audio("./sounds/tom-4.mp3");
+        d.play();
+      break;
+
+    case "j":
+        let j = new Audio("./sounds/snare.mp3");
+        j.play();
+      break;
+
+    case "k":
+        let k = new Audio("./sounds/crash.mp3");
+        k.play();
+      break;
+
+    case "l":
+        let l = new Audio("./sounds/kick-bass.mp3");    
+        l.play();
+      break;
+
+    default:  
+      console.log(event.key);
+      break;
+  }
+});
