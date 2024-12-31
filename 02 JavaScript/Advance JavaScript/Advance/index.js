7aa108e33b87aebeb19d7b69218dea0fcb10af5d
Priyanshu-010 <priyanshurai2772@gmail.com>
2024-12-31 16:00:44 +0530

    Objects

--- 02 JavaScript/Advance JavaScript/Advance/index.js
+++ 02 JavaScript/Advance JavaScript/Advance/index.js
@@ -5,7 +5,12 @@ function BellBoy(name, age, hasWorkPermit, languages){
   this.age = age;
   this.hasWorkPermit = hasWorkPermit;
   this.languages = languages;
+  this.clean = function(){
+    console.log("Cleaning in progress");
+  }
 }
 
+// const bellBoy1 = new BellBoy("John", 25, true, ["Engish", "French"]);
 const bellBoy1 = new BellBoy("John", 25, true, ["Engish", "French"]);
-console.log(bellBoy1)
+console.log(bellBoy1.name);
+bellBoy1.clean();
