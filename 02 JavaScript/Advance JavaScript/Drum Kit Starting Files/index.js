ca63504ca9488173a0a3a7f3341e2b6d07c1bb59
Priyanshu-010 <priyanshurai2772@gmail.com>
2024-12-31 23:18:49 +0530

    Completed The drum kit project and the advance JavaScript module

--- 02 JavaScript/Advance JavaScript/Drum Kit Starting Files/index.js
+++ 02 JavaScript/Advance JavaScript/Drum Kit Starting Files/index.js
@@ -117,58 +117,22 @@ for (let i = 0; i <drum; i++){
     // let audio = new Audio("./sounds/" + this.innerHTML + ".mp3");
     // audio.play();
     // this.style.color = "white"; 
-    // console.log(this.innerHTML);
+    console.log(this);
     let buttonInnerHTML = this.innerHTML;
+    makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
 
-    switch (buttonInnerHTML) {
-      case "w":
-          let w = new Audio("./sounds/tom-1.mp3");
-          w.play();
-        break;
-       
-      case "a":
-          let a = new Audio("./sounds/tom-2.mp3");
-          a.play();
-        break;
-
-      case "s":
-          let s = new Audio("./sounds/tom-3.mp3");
-          s.play();
-        break;   
-
-      case "d":
-          let d = new Audio("./sounds/tom-4.mp3");
-          d.play();
-        break;
-
-      case "j":
-          let j = new Audio("./sounds/snare.mp3");
-          j.play();  
-        break;
-
-      case "k":
-          let k = new Audio("./sounds/crash.mp3");
-          k.play();
-        break;
-
-      case "l":
-          let l = new Audio("./sounds/kick-bass.mp3");
-          l.play();
-        break;        
-    
-      default:
-        console.log(buttonInnerHTML);
-        break;
-    }
-  });
-
-
-  }
+})};
 
 
 document.addEventListener("keydown", function(event){
   // console.log(event)
-  switch (event.key) {
+  makeSound(event.key);
+  buttonAnimation(event.key);
+})
+
+function makeSound(key){
+  switch (key) {
     case "w":
         let w = new Audio("./sounds/tom-1.mp3");
         w.play();
@@ -205,7 +169,14 @@ document.addEventListener("keydown", function(event){
       break;
 
     default:  
-      console.log(event.key);
+      console.log(key);
       break;
-  }
-});
+}
+}
+function buttonAnimation(currentKey){
+  let activeButton = document.querySelector("." + currentKey)
+  activeButton.classList.add("pressed");
+  setTimeout(function(){
+    activeButton.classList.remove("pressed");
+  }, 100);
+}
