eed2ffc7903776507e2371dd5a5f49f3c57e3ce7
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-01-04 22:50:34 +0530

    Backend Express and node js 3.2

---
+++ 03 Backend/Express and Node/3.2 HTTP Requests/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+const app = express();
+const port = 3000;
+
+
+app.get("/",(req,res)=>{
+  res.send(("Hello World!! This is the Home Page"))
+})
+
+app.get("/about",(req,res)=>{
+  res.send("About Page")
+})
+app.get("/contact",(req,res)=>{
+  res.send("Contact Page")
+})
+
+app.listen(port, ()=>{
+  console.log(`Server running on port ${port}.`);
+})
