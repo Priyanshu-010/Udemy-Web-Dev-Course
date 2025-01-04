06ad710aa4035f5030c85f3b03f3347d837e4c86
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-01-04 16:03:00 +0530

    2.3

--- Backend/Node Js/2.3 NPM/index.js
+++ Backend/Node Js/2.3 NPM/index.js
@@ -1,3 +1,7 @@
-const sillyname = require("sillyname");
-const name = sillyname();
-console.log(`My name is ${name}.`);
+// import sillyname from "sillyname";
+// const name = sillyname();
+// console.log(`My name is ${name}.`);
+
+import superheroes from "superheroes";
+const name = superheroes.random();
+console.log(`I am ${name}!`);
