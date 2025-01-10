95b687a0413f9fd7e9b96d955b41336a71fe0424
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-01-10 16:10:52 +0530

    4.0 ejs completed

--- 03 Backend/04 EJS/4.0 EJS/mysoln/index.js
+++ 03 Backend/04 EJS/4.0 EJS/mysoln/index.js
@@ -2,9 +2,25 @@ import express from "express";
 
 const app = express();
 const port = 3000;
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.send("Hello, EJS!");
+  const today = new Date();
+  let day = today.getDay();
+  // console.log(day)
+
+  let type = "a weekday";
+  let adv = "it's time to work hard";
+
+  if (day === 0 || day === 6){
+    type = "the weekend";
+    adv = "it's time to have some fun";
+  }
+
+  res.render("index.ejs", {
+    dayType: type,
+    advice: adv,
+  });
 });
 
 app.listen(port, () => {
